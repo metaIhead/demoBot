7510e3d01c8abe77571e76c66865cfbb5666a20c
metaIhead <kuziashp@gmail.com>
2020-03-12 18:31:08 +0800

    Update web.py

--- web.py
+++ web.py
@@ -20,7 +20,7 @@ if "HEROKU" in list(os.environ.keys()):
     @server.route("/")
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url="https://bot-key.herokuapp.com/web") # этот url нужно заменить на url вашего Хероку приложения
+        bot.set_webhook(url="https://bot-key.herokuapp.com") # этот url нужно заменить на url вашего Хероку приложения
         return "?", 200
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
 else:

481190accf3081d11b4177a575698f546835eb6c
metaIhead <kuziashp@gmail.com>
2020-03-13 12:53:16 +0800

    Update try.py

--- try.py
+++ try.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask ,request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    print(request.values)
     return "Hello World!"
 
+
 # if __name__ == "__main__":
-#     app.run()
+#     app.run(debug=True)

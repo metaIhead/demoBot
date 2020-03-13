bd0b1907e2c424a43972150ba0d738eb60608c97
metaIhead <kuziashp@gmail.com>
2020-03-13 13:00:24 +0800

    Update try.py

--- try.py
+++ try.py
@@ -1,7 +1,7 @@
 from flask import Flask ,request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/1128488996:AAHIwMHnJoq85VhgUMncZ9295HNmhTNPNH0")
 def hello():
     print(request.values)
     return "Hello World!"
@@ -9,3 +9,11 @@ def hello():
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
+
+
+
+
+# https://api.telegram.org/bot1128488996:AAHIwMHnJoq85VhgUMncZ9295HNmhTNPNH0/setWebhook?url=https://bot-key.herokuapp.com/1128488996:AAHIwMHnJoq85VhgUMncZ9295HNmhTNPNH0
+
+
+# https://api.telegram.org/bot1128488996:AAHIwMHnJoq85VhgUMncZ9295HNmhTNPNH0/deleteWebhook

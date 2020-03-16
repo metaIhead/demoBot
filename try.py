cdb14380f7c9c27f58ce9561327dc634b036fda7
metaIhead <kuziashp@gmail.com>
2020-03-16 12:08:38 +0800

    Update try.py

--- try.py
+++ try.py
@@ -6,29 +6,27 @@ import datetime as d
 import transliterate
 from keyboards import *
 
-
-token="1128488996:AAHIwMHnJoq85VhgUMncZ9295HNmhTNPNH0"
-app = Flask(__name__)
-
-@app.route("/"+token, methods=['POST'])
-def get_response():
-    response=request.json
-
-    return "Hello World!"
-
-print(response)
-bot = telebot.TeleBot(token)
-# message=response
-#Обработчик команд '/start' и '/help'.
-@bot.message_handler(commands=['start'])
-def handle_start_help(message):
-    print("start")
-
-
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-
+def handler(message):
     print(message.chat.id)
     print("______")
     bot.send_message(message.chat.id,message.chat.id)
     main_menu(message)
+
+
+token="1128488996:AAHIwMHnJoq85VhgUMncZ9295HNmhTNPNH0"
+app = Flask(__name__)
+
+bot = telebot.TeleBot(token)
+
+def handler(message):
+    print(message.chat.id)
+    print("______")
+    bot.send_message(message.chat.id,message.chat.id)
+    main_menu(message)
+
+@app.route("/"+token, methods=['POST'])
+def get_response():
+    response = request.json
+    message = response
+    handler(message)
+    return "Hello World!"
